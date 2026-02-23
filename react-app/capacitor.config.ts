7889685c326a654002d031add3a364a981cf079a
Metaheurist <Onceuponablock@hotmail.com>
2026-02-23 08:21:09 +0000

    Cleaned up code

--- react-app/capacitor.config.ts
+++ react-app/capacitor.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="@capacitor/local-notifications" />
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
@@ -9,6 +10,11 @@ const config: CapacitorConfig = {
   },
   android: {
     allowMixedContent: true,
+  },
+  plugins: {
+    LocalNotifications: {
+      iconColor: '#488AFF',
+    },
   },
 };
 
